24fb8f3fbb9822e3b24dd63fc857ed3cfe0e603a
Candy <mei.fong.yuen@gmail.com>
2021-03-30 14:48:13 +0100

    further exploration - refactered to word wrap long messages

--- RB101/small_problems/easy05/easy5-10.rb
+++ RB101/small_problems/easy05/easy5-10.rb
@@ -33,5 +33,33 @@ def print_in_box(text)
 
 end
 
-print_in_box('To boldly go where no one has gone before.')
-print_in_box('')
+#Further exploration
+
+def print_in_box_wordwrap(text)
+  if text.length <= 76
+    print_in_box(text)
+
+  else #when text.length > 76
+    whole_line = "+-" + "-" * 76 + "-+"
+    empty_line = "| " + " " * 76 + " |"
+
+    puts whole_line
+    puts empty_line
+
+    current_text_line = text.dup #use .dup to avoid original text to be altered.
+
+    while current_text_line.length > 76 #loop to print 1st to 2nd last line of the input text
+      puts "| " + current_text_line.slice!(0,76) + " |"
+    end
+
+    puts  "| " + current_text_line + " " * (76 - current_text_line.length) + " |" 
+    #print the last line of the text, add space to keep the whole box width as 80
+    puts empty_line
+    puts whole_line
+  end
+end
+
+
+print_in_box_wordwrap('To boldly go where no one has gone before.')
+print_in_box_wordwrap('')
+print_in_box_wordwrap("This is a very long messsage to be chopped into different lines. This is a very long messsage to be chopped into different lines. This is a very long messsage to be chopped into different lines.")
