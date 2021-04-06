b919efbcd59570b21293587ec227e504ead27143
Candy <mei.fong.yuen@gmail.com>
2021-04-06 12:37:50 +0100

    add solution

---
+++ RB101/small_problems/easy06/easy6-04.rb
@@ -0,0 +1,68 @@
+
+# Reversed Arrays (Part 1)
+# Write a method that takes an Array as an argument, and reverses its elements in place; that is, mutate the Array passed into this method. The return value should be the same Array object.
+
+# You may not use Array#reverse or Array#reverse!.
+
+# Examples:
+
+# list = [1,2,3,4]
+# result = reverse!(list)
+# result == [4, 3, 2, 1] # true
+# list == [4, 3, 2, 1] # true
+# list.object_id == result.object_id # true
+
+# list = %w(a b e d c)
+# reverse!(list) == ["c", "d", "e", "b", "a"] # true
+# list == ["c", "d", "e", "b", "a"] # true
+
+# list = ['abc']
+# reverse!(list) == ["abc"] # true
+# list == ["abc"] # true
+
+# list = []
+# reverse!(list) == [] # true
+# list == [] # true
+
+# Note: for the test case list = ['abc'], we want to reverse the elements in the array. The array only has one element, a String, but we're not reversing the String itself, so the reverse! method call should return ['abc'].
+
+
+#In this method, the last element (in the original position) in list remains unchanged; instead, at each iteration the element before it is being removed and pushed at the end of the array until the first element (in the original position) is also moved.
+
+def reverse!(list)
+  index = -2
+
+  loop do 
+    break if index < -list.length 
+    list << list.delete_at(index)
+    index -= 1
+  end
+
+  list
+end
+
+list = [1,2,3,4]
+result = reverse!(list)
+puts result == [4, 3, 2, 1] # true
+puts list == [4, 3, 2, 1] # true
+puts list.object_id == result.object_id # true
+
+list = %w(a b e d c)
+puts reverse!(list) == ["c", "d", "e", "b", "a"] # true
+puts list == ["c", "d", "e", "b", "a"] # true
+
+list = ['abc']
+puts reverse!(list) == ["abc"] # true
+puts list == ["abc"] # true
+
+list = []
+puts reverse!(list) == [] # true
+puts list == [] # true
+
+list = [1,2,3,2]
+result = reverse!(list)
+puts result == [2, 3, 2, 1] # true
+puts list == [2, 3, 2, 1] # true
+puts list.object_id == result.object_id # true
+
+
