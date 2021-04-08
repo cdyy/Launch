d0a4ceef6133fa9a5bf6e676f0785214e90c81d1
Candy <mei.fong.yuen@gmail.com>
2021-04-08 23:46:02 +0100

    add solution

---
+++ RB101/small_problems/easy06/easy6-10.rb
@@ -0,0 +1,36 @@
+# Right Triangles
+# Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+# Examples:
+
+# triangle(5)
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+# triangle(9)
+
+#         *
+#        **
+#       ***
+#      ****
+#     *****
+#    ******
+#   *******
+#  ********
+# *********
+
+def triangle(n)
+  index = 1
+  loop do
+    puts " " * (n - index) + "*" * index 
+    break if index == n
+    index += 1
+  end
+end
+
+triangle(5)
+triangle(9)
