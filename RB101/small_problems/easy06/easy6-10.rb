8505c1fd3e05b085ec83e1ec4696e4e63426f7fa
Candy <mei.fong.yuen@gmail.com>
2021-04-10 22:07:16 +0100

    further exploration 2: add my solution (first try)

--- RB101/small_problems/easy06/easy6-10.rb
+++ RB101/small_problems/easy06/easy6-10.rb
@@ -34,6 +34,9 @@ def triangle(n)
   end
 end
 
+# Further Exploration
+# Try modifying your solution so it prints the triangle upside down from its current orientation.
+
 def triangle_topright(n)
   stars = n
   spaces = 0
@@ -44,6 +47,60 @@ def triangle_topright(n)
     spaces += 1
   end
 end
+
+# Try modifying your solution again so that you can display the triangle with the right angle at any corner of the grid.
+
+def triangle_bottom(n,direction)
+  stars = 1
+  spaces = n - 1
+
+  n.times do |i|
+    
+    left = "*" * stars + " " * spaces
+    right = " " * spaces + "*" * stars
+
+    line = direction[-1] == "l" ? left : right
+    puts line
+
+    stars += 1
+    spaces -= 1
+  end
+end
+
+def triangle_top(n,direction)
+  stars = n
+  spaces = 0
+
+  n.times do |i|
+
+    left = "*" * stars + " " * spaces
+    right = " " * spaces + "*" * stars
+
+    line = direction[-1] == "l" ? left : right
+    puts line
+      
+    stars -= 1
+    spaces += 1
+  end
+end
+
+def triangle_alldirections(n, direction) 
+#top-r, top-l, bottom-r, bottom-l
+  if direction[0..2] == "top"
+    triangle_top(n,direction)
+  elsif direction[0..5] == "bottom"
+    triangle_bottom(n,direction)
+  end
+end
+
+triangle_alldirections(10, "top-r") 
+triangle_alldirections(10, "top-l") 
+triangle_alldirections(10, "bottom-r") 
+triangle_alldirections(10, "bottom-l") 
+
+
+
+
 
 
 
