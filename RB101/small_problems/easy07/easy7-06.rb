042a9c92a18cda75f86cefdcd338bb8539d5025f
Candy <mei.fong.yuen@gmail.com>
2021-04-17 22:35:00 +0100

    further exploration: add my solution

--- RB101/small_problems/easy07/easy7-06.rb
+++ RB101/small_problems/easy07/easy7-06.rb
@@ -20,3 +20,55 @@ def staggered_case(string)
     end
   end.join
 end
+
+#Further exploration
+
+# Further Exploration
+# Modify this method so the caller can determine whether non-alphabetic characters should be counted when determining the upper/lowercase state. That is, you want a method that can perform the same actions that this method does, or operates like the previous version.
+
+# Hint: Use a keyword argument.
+
+
+def staggered_case_not_count_non_alphabetic(string)
+  need_upper = true
+  string.split('').map do |char| 
+    if char =~ /[a-zA-z]/
+      transformed_char = need_upper ? char.upcase : char.downcase
+      need_upper = !need_upper
+      transformed_char
+    else
+      char
+    end
+  end.join
+end
+
+def staggered_case_count_non_alphabetic(string)
+  need_upper = true
+  string.split('').map do |char| 
+    transformed_char = need_upper ? char.upcase : char.downcase
+    need_upper = !need_upper
+    transformed_char
+  end.join
+end
+
+def staggered_case3(string, count_non_alphabetic = true)
+  if count_non_alphabetic == true
+    staggered_case_count_non_alphabetic(string) 
+  else
+    staggered_case_not_count_non_alphabetic(string)
+  end
+end
+
+staggered_case3('I Love Launch School!', true)
+staggered_case3('I Love Launch School!', false)
+staggered_case3('ALL CAPS', true)
+staggered_case3('ALL CAPS', false)
+staggered_case3('ignore 77 the 444 numbers', true)
+staggered_case3('ignore 77 the 444 numbers', false)
+
+
+
+
+
+
+
