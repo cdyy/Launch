1c083d77eca4de1320f23d3f76aa4d3d4cfe4d98
Candy <mei.fong.yuen@gmail.com>
2021-04-20 22:03:19 +0100

    add solution

---
+++ RB101/small_problems/easy07/easy7-09.rb
@@ -0,0 +1,16 @@
+# Multiply All Pairs
+# Write a method that takes two Array arguments in which each Array contains a list of numbers, and returns a new Array that contains the product of every pair of numbers that can be formed between the elements of the two Arrays. The results should be sorted by increasing value.
+
+# You may assume that neither argument is an empty Array.
+
+# Examples:
+
+# multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+
+def multiply_all_pairs(array1, array2)
+  products = []
+  array1.each do |num|
+    array2.size.times {|i| products << num * array2[i]}
+  end
+  products.sort
+end
