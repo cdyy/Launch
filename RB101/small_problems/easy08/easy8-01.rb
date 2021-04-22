c6214e8dee968b253799527a9cbec156046d2ee4
Candy <mei.fong.yuen@gmail.com>
2021-04-22 20:35:10 +0100

    add solution

--- RB101/small_problems/easy08/easy8-01.rb
+++ RB101/small_problems/easy08/easy8-01.rb
@@ -8,6 +8,16 @@
 # sum_of_sums([4]) == 4
 # sum_of_sums([1, 2, 3, 4, 5]) == 35
 
+# solution 1
+# array = [3, 5, 2]
+# result = the sum of 
+# 3
+# 3 + 5
+# 3 + 5 + 2
+
+# 3 (array[0]) appears 3 times (= array size - 0)
+# 5 (array[1]) apears 2 times (= array size - 1)
+# 5 (array[2]) apears 1 times (= array size - 2)
 
 def sum_of_sums(array)
   result = 0
@@ -16,3 +26,24 @@ def sum_of_sums(array)
   end
   result
 end
+
+
+
+# solution 2
+
+# result = 0
+# sum of array[0..0] => add to result
+# sum of array[0..1] => add to result
+# sum of array[0..2] => add to result
+# ...sum of array[0..until at end of array) => add to result
+
+def sum_of_sums(array)
+  result = 0
+  array.each_index do |i|
+    result += array[0..i].sum
+  end
+  result
+end
+
+
+
