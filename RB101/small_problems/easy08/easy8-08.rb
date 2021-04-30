0945ed05e4ca05bd7234fb87560b0ae0378a67c3
Candy <mei.fong.yuen@gmail.com>
2021-04-30 23:58:17 +0100

    add solution

---
+++ RB101/small_problems/easy08/easy8-08.rb
@@ -0,0 +1,16 @@
+# Double Char (Part 2)
+# Write a method that takes a string, and returns a new string in which every consonant character is doubled. Vowels (a,e,i,o,u), digits, punctuation, and whitespace should not be doubled.
+
+# Examples:
+
+# double_consonants('String') == "SSttrrinngg"
+# double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+# double_consonants("July 4th") == "JJullyy 4tthh"
+# double_consonants('') == ""
+
+
+def double_consonants(string)
+  string.split('').map do |char| 
+    char.match(/^aeiou/i) ? char * 2 : char
+  end.join
+end
