eda2331e7ac00beeb25c9a44956fe7258760e6a8
Candy <mei.fong.yuen@gmail.com>
2021-05-07 17:38:42 +0100

    add my solution

---
+++ RB101/small_problems/easy09/easy9-05.rb
@@ -0,0 +1,24 @@
+
+# Uppercase Check
+# Write a method that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. Characters that are not alphabetic should be ignored.
+
+# Examples:
+
+# uppercase?('t') == false
+# uppercase?('T') == true
+# uppercase?('Four Score') == false
+# uppercase?('FOUR SCORE') == true
+# uppercase?('4SCORE!') == true
+# uppercase?('') == true
+
+# my solution
+# - split the string into array of characters
+# - count how many characters are downcase
+# - return false if number of downcase characters is greater than 0, otherwise return true
+# - for non-alphabetic characters the selection criteria char.upcase != char will return true so they will not interfere the result
+
+def uppercase?(string)
+  characters_arr = string.split('')
+  downcase_total = characters_arr.count {|char| char.upcase != char}
+  downcase_total > 0 ? false : true
+end
