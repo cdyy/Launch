6722fb02f26ac3fefb48724277b295237466ce14
Candy <mei.fong.yuen@gmail.com>
2021-05-13 23:46:02 +0100

    add a method to check if two words are anagrams

---
+++ RB101/small_problems/easy09/easy9-11.rb
@@ -0,0 +1,15 @@
+# Group Anagrams
+# Given the array...
+
+# words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+#           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+#           'flow', 'neon']
+# Write a program that prints out groups of words that are anagrams. Anagrams are words that have the same exact letters in them but in a different order. Your output should look something like this:
+
+# ["demo", "dome", "mode"]
+# ["neon", "none"]
+# #(etc)
+
+def is_anagram?(word1, word2)
+  word1.split('').sort == word2.split('').sort
+end
